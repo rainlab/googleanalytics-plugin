e256326bcd7f8c320eabede69cece3e4dd0e9163
Sam Georges <sam@daftspunk.com>
2014-07-12 17:52:48 +1000

    Minor fix

--- models/Settings.php
+++ models/Settings.php
@@ -11,6 +11,8 @@ use October\Rain\Database\Model;
  */
 class Settings extends Model
 {
+    use \October\Rain\Database\Traits\Validation;
+
     public $implement = ['System.Behaviors.SettingsModel'];
 
     public $settingsCode = 'rainlab_googleanalytics_settings';
