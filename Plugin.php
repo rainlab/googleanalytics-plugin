0be7c199a7bd11da81ae58a217b080fff8227bef
Talha Zekeriya Durmuş <drtzack@gmail.com>
2015-09-20 14:32:41 +0300

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -13,7 +13,7 @@ class Plugin extends PluginBase
     {
         return [
             'name'        => 'Google Analytics',
-            'description' => 'rainlab.googleanalytics::lang.strings.plugin_desc',
+            'description' => 'Provides the Google Analytics tracking and reporting.',
             'author'      => 'Alexey Bobkov, Samuel Georges',
             'icon'        => 'icon-bar-chart-o'
         ];
@@ -23,16 +23,6 @@ class Plugin extends PluginBase
     {
         return [
             '\RainLab\GoogleAnalytics\Components\Tracker' => 'googleTracker'
-        ];
-    }
-
-    public function registerPermissions()
-    {
-        return [
-            'rainlab.googleanalytics.access_settings' => [
-                'tab'   => 'rainlab.googleanalytics::lang.permissions.tab',
-                'label' => 'rainlab.googleanalytics::lang.permissions.settings',
-            ],
         ];
     }
 
@@ -68,11 +58,18 @@ class Plugin extends PluginBase
             'config' => [
                 'label'       => 'Google Analytics',
                 'icon'        => 'icon-bar-chart-o',
-                'description' => 'rainlab.googleanalytics::lang.strings.settings_desc',
+                'description' => 'Configure Google Analytics API code and tracking options.',
                 'class'       => 'RainLab\GoogleAnalytics\Models\Settings',
-                'permissions' => ['rainlab.googleanalytics.settings'],
-                'order'       => 600
+                'order'       => 600,
+                'permissions' => ['rainlab.googleanalytics.access_settings'],
             ]
+        ];
+    }
+
+    public function registerPermissions()
+    {
+        return [
+            'rainlab.googleanalytics.access_settings' => ['tab' => 'Analytics', 'label' => 'Access Settings'],
         ];
     }
 
