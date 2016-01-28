3e105c139868527066a733b8d9eb3b4896ea4dfd
Samuel Georges <sam@daftspunk.com>
2016-01-28 17:03:39 +1100

    Merge general+strings lang group

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -11,14 +11,10 @@ return [
         'title_traffic_sources' => 'Verwijzende sites',
         'description_traffic_sources' => 'Toont verwijzende sites die een link hebben naar je website.',
     ],
-    'general' => [
+    'strings' => [
         'page_url' => 'Pagina URL',
         'pageviews' => 'Paginaweergaves',
         'current' => 'Huidig',
         'goal' => 'Goal'
     ],
-    'exceptions' => [
-        'google_api_not_configured' => 'Google Analytics API is niet geconfigureerd. Ga naar Instellingen / Google Analytics om te configureren',
-        'google_api_key_not_uploaded' => 'Google Analytics API private key is niet geupload. Ga naar Instellingen / Google Analytics om de key te uploaden.'
-    ]
 ];
