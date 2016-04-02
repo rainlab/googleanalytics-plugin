ca6ba1ec17eac5fa9c1b1d1b1cb291ea1c1e22d3
Alexandre GADIOU <alexandregadiou44@icloud.com>
2016-04-02 19:29:35 +0200

    add a blacklist
    hide ghost spam with a list of sources with ';' delimiter

--- reportwidgets/TrafficSources.php
+++ reportwidgets/TrafficSources.php
@@ -1,4 +1,6 @@
-<?php namespace RainLab\GoogleAnalytics\ReportWidgets;
+<?php
+
+namespace RainLab\GoogleAnalytics\ReportWidgets;
 
 use Backend\Classes\ReportWidgetBase;
 use RainLab\GoogleAnalytics\Classes\Analytics;
@@ -13,15 +15,17 @@ use Exception;
  */
 class TrafficSources extends ReportWidgetBase
 {
+
     /**
      * Renders the widget.
      */
     public function render()
     {
-        try {
+        try
+        {
             $this->loadData();
-        }
-        catch (Exception $ex) {
+        } catch (Exception $ex)
+        {
             $this->vars['error'] = $ex->getMessage();
         }
 
@@ -32,44 +36,50 @@ class TrafficSources extends ReportWidgetBase
     {
         return [
             'title' => [
-                'title'             => 'backend::lang.dashboard.widget_title_label',
-                'default'           => e(trans('rainlab.googleanalytics::lang.widgets.title_traffic_sources')),
-                'type'              => 'string',
+                'title' => 'backend::lang.dashboard.widget_title_label',
+                'default' => e(trans('rainlab.googleanalytics::lang.widgets.title_traffic_sources')),
+                'type' => 'string',
                 'validationPattern' => '^.+$',
                 'validationMessage' => 'backend::lang.dashboard.widget_title_error'
             ],
             'reportSize' => [
-                'title'             => 'Chart radius',
-                'default'           => '150',
-                'type'              => 'string',
+                'title' => 'Chart radius',
+                'default' => '150',
+                'type' => 'string',
                 'validationPattern' => '^[0-9]+$',
                 'validationMessage' => 'Please specify the chart size as an integer value.'
             ],
             'center' => [
-                'title'             => 'Center the chart',
-                'type'              => 'checkbox'
+                'title' => 'Center the chart',
+                'type' => 'checkbox'
             ],
             'legendAsTable' => [
-                'title'             => 'Display legend as a table',
-                'type'              => 'checkbox',
-                'default'           => 1
+                'title' => 'Display legend as a table',
+                'type' => 'checkbox',
+                'default' => 1
             ],
             'days' => [
-                'title'             => 'Number of days to display data for',
-                'default'           => '30',
-                'type'              => 'string',
+                'title' => 'Number of days to display data for',
+                'default' => '30',
+                'type' => 'string',
                 'validationPattern' => '^[0-9]+$'
             ],
             'number' => [
-                'title'             => 'Number of sources to display',
-                'default'           => '10',
-                'type'              => 'string',
+                'title' => 'Number of sources to display',
+                'default' => '10',
+                'type' => 'string',
                 'validationPattern' => '^[0-9]+$'
             ],
             'displayDescription' => [
-                'title'             => 'Display the report description',
-                'type'              => 'checkbox',
-                'default'           => 1
+                'title' => 'Display the report description',
+                'type' => 'checkbox',
+                'default' => 1
+            ],
+            'blacklist' => [
+                'title' => 'Hide sources',
+                'type' => 'string',
+                'description' => 'add sources to hide with ; delimiter',
+                'default' => 'traffic2cash.xyz;trafficgenius.xyz'
             ]
         ];
     }
@@ -78,20 +88,32 @@ class TrafficSources extends ReportWidgetBase
     {
         $days = $this->property('days');
         if (!$days)
-            throw new ApplicationException('Invalid days value: '.$days);
+            throw new ApplicationException('Invalid days value: ' . $days);
 
         $obj = Analytics::instance();
         $data = $obj->service->data_ga->get(
-            $obj->viewId,
-            $days.'daysAgo',
-            'today',
-            'ga:visits',
-            ['dimensions' => 'ga:source', 'sort' => '-ga:visits']
+                $obj->viewId, $days . 'daysAgo', 'today', 'ga:visits', ['dimensions' => 'ga:source', 'sort' => '-ga:visits']
         );
 
-        $rows = $data->getRows() ?: [];
+        $rows = $data->getRows() ? : [];
 
-        $this->vars['rows'] = array_slice($rows, 0, $this->property('number'));
-        $this->vars['total'] = $data->getTotalsForAllResults()['ga:visits'];
+        $theblacklist = explode(";", $this->property('blacklist'));
+        $filtered_rows = array();
+        $filtered_visit = 0;
+        foreach ($rows as $row)
+        {
+            if (!in_array($row[0], $theblacklist))
+            {
+                array_push($filtered_rows, $row);
+            }
+            else
+            {
+                $filtered_visit = $filtered_visit + $row[1];
+            }
+        }
+
+        $this->vars['rows'] = array_slice($filtered_rows, 0, $this->property('number'));
+        $this->vars['total'] = $data->getTotalsForAllResults()['ga:visits'] - $filtered_visit;
     }
+
 }
