08bcdcba3bcf5d16ab07208311a9291e8fc4fc20
Anne Datema <info@annedatema.nl>
2017-08-20 17:31:55 +0200

    fix #66 Ease configuration requirements for wider use

--- models/Settings.php
+++ models/Settings.php
@@ -27,8 +27,8 @@ class Settings extends Model
      * Validation rules
      */
     public $rules = [
-        'gapi_key'   => 'required',
-        'profile_id'   => 'required'
+        'gapi_key'   => 'required_with:profile_id',
+        'profile_id'   => 'required_with:gapi_key'
     ];
 
     public function initSettingsData()
